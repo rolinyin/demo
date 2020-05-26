5be3535c1100ed36288839f8d29cc9ef91bef22a
rolinyin <rolinyin@163.com>
2020-05-26 23:58:58 +0800

    添加mockito的使用示例

--- src/main/java/com/yonyou/demo/redis/controller/OrderController.java
+++ src/main/java/com/yonyou/demo/redis/controller/OrderController.java
@@ -1,5 +1,8 @@
 package com.yonyou.demo.redis.controller;
 
+import com.yonyou.demo.redis.api.OrderServiceI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/order")
 public class OrderController {
 
+    @Autowired
+    private OrderServiceI service;
+
+    @GetMapping(value = "/orders")
+    public Object getAllOrders(){
+
+        return service.getAllOrders();
+    }
 }
--- src/test/java/com/yonyou/demo/redis/service/MockitoTest.java
+++ src/test/java/com/yonyou/demo/redis/service/MockitoTest.java
@@ -4,12 +4,23 @@ import com.yonyou.demo.TestApplication;
 import com.yonyou.demo.redis.api.OrderServiceI;
 import com.yonyou.demo.redis.entity.Order;
 import com.yonyou.demo.redis.entity.Product;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.UnsupportedEncodingException;
 
 import static org.mockito.ArgumentMatchers.anyString;
 
@@ -25,6 +36,10 @@ public class MockitoTest {
 
     @MockBean
     private OrderServiceI service;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
 
     //mock 对象
     @Test
@@ -51,5 +66,26 @@ public class MockitoTest {
         Mockito.doReturn(order).when(service).getById(anyString());
         Order result = service.getById("123");
         System.out.println(result.getName());
+    }
+
+    @Test
+    public void testRequest() throws Exception {
+        // perform : 执行请求 ;
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                //MockMvcRequestBuilders.post("/url") ： 构造一个post请求
+                .post("/user/insert")
+                .accept(MediaType.APPLICATION_JSON)
+                //传参,因为后端是@RequestBody所以这里直接传json字符串
+                //.content(jsonResult)
+                // 请求type : json
+                .contentType(MediaType.APPLICATION_JSON))
+                // 期望的结果状态 200
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();//返回结果
+
+        int statusCode = mvcResult.getResponse().getStatus();
+        String result = mvcResult.getResponse().getContentAsString();
+        Assert.assertEquals(statusCode,"200");
+        System.out.println(result);
     }
 }
